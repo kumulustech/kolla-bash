70d384eea2207b55262c4e17d7f8356aaea87e05
Robert Starmer <rstarmer@gmail.com>
2016-10-02 14:23:01 -0700

    add https apt support

--- debian-cmp.sh
+++ debian-cmp.sh
@@ -30,6 +30,7 @@ apt install \
 apt-get purge lxc lxd -y
 pip install -U pip
 
+apt-get install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 deb https://apt.dockerproject.org/repo ubuntu-xenial main
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
