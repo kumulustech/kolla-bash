b0667e20ee628c42f2bc0e20f0e82937bda63a0b
Robert Starmer <rstarmer@gmail.com>
2016-10-03 10:56:27 -0700

    update flavor script

--- add_flavor.sh
+++ add_flavor.sh
@@ -2,7 +2,7 @@
 
 source ~/open.rc
 
-nova flavor-create --is-public True m1.small 3 512 10 1
-nova flavor-create --is-public True m1.medium 5 1024 10 2
-nova flavor-create --is-public True m1.medium 7 4096 10 4
+openstack flavor create --public  --id  3 --ram 512 --disk 10 --vcpus 1 m1.small
+openstack flavor create --public  --id  5 --ram 1024 --disk 10 --vcpus 2 m1.medium
+openstack flavor create --public  --id  7 --ram 2048 --disk 10 --vcpus 2 m1.large
 
