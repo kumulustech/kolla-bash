ae2649ee8f669cd152b4b6c108beb4009c7f00af
Robert Starmer <robertstarmer@gmail.com>
2016-10-21 03:09:25 +0000

    fix basic multinode start script

--- multinode.sh
+++ multinode.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-if [ $# -le "2" ] ; then
+set -x
+if [ $# -le "1" ] ; then
  echo "Usage: $0 control_ip_or_hostname compute_ip_or_hostname"
  exit 1
 fi
 
 if [ ! -f ~/.ssh/id_rsa ] ; then
-
+exit 1
 fi
-cat multinode.tmpl | sed -i 's/#__CONTROL__/${1}/' >/tmp/multinode_cmp.tmpl
-cat /tmp/multinode_cmp.tmpl | sed =i 's/#__COMPUTE__/${2}\n#_COMPUTE__/' > multinode
+cat ./multinode.tmpl | sed -e "s/#__CONTROL__/${1}/" >/tmp/multinode_cmp.tmpl
+cat /tmp/multinode_cmp.tmpl | sed -e "s/#__COMPUTE__/${2}\n#__COMPUTE__/" > multinode
 rm /tmp/multinode_cmp.tmpl
 
-echo "Run: kolla-ansible -i multinode deploy
+echo "Run: kolla-ansible -i multinode deploy"
 
